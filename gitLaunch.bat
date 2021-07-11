5c914fe7a3532577e1bd4ef7f157071d5509558d
94Central <94gamingcentral@gmail.com>
2021-07-10 20:37:14 -0500

    third

---
+++ gitLaunch.bat
@@ -0,0 +1,7 @@
+echo "# IdiotBot" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/94Central/IdiotBot.git
+git push -u origin main
